83dcd9bd03c2e7a84444d3c1ff64fb4737ec4645
RaulFeier <ionelafeier1@gmail.com>
2024-02-07 22:22:27 +0200

    eep

--- main.cpp
+++ main.cpp
@@ -4,13 +4,6 @@ using namespace std;
 using i64 = long long;
 
 int main() {
-  i64 x = 3;
-
-  if (x == 3) {
-    cout << "Mata" << endl;
-  } else {
-    cout << "Tata" << endl;
-  }
 
   cout << "Hello, World!" << endl;
 
