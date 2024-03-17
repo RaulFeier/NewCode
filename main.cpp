fc55e90f39479fe38948428a805ae0d166d25641
RaulFeier <ionelafeier1@gmail.com>
2024-03-17 12:06:07 +0200

    commit from windows

--- main.cpp
+++ main.cpp
@@ -7,8 +7,8 @@ int main() {
 
   cout << "Hello, World!" << endl;
 
-  for (i64 i = 0; i < 10; i++) {
-    cout << "Hello, World" << endl;
+  for (int i = 0; i < 100; i++) {
+    cout << "EEP" << endl;
   }
 
   return 0;
